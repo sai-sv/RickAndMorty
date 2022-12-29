156c608e58f8ad9a4d0868920a08d73bb88c9f2d
sergei <sergei.sai@bk.ru>
2022-12-30 00:47:04 +0300

    Часть 2
    Добавлены комментарии и API файлы

---
+++ RickAndMorty/APIClients/RMRequest.swift
@@ -0,0 +1,13 @@
+//
+//  RMRequest.swift
+//  RickAndMorty
+//
+//  Created by Sergei Sai on 30.12.2022.
+//
+
+import Foundation
+
+/// Object that represents a single API call
+final class RMRequest {
+    
+}
