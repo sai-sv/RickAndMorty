e540e652d6493bfff8ad5ca8b0270ca4c8cbd2d0
sergei <sergei.sai@bk.ru>
2022-12-29 23:35:59 +0300

    init

---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Sergei Sai on 29.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
