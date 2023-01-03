6f63af1729e5e003f20206715f70fe4fda86f2c5
sergei <sergei.sai@bk.ru>
2023-01-03 23:06:32 +0300

    Часть 4
    Формирование URL запроса

--- RickAndMorty/APIClients/RMRequest.swift
+++ RickAndMorty/APIClients/RMRequest.swift
@@ -7,7 +7,50 @@
 
 import Foundation
 
+enum HttpMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+}
+
 /// Object that represents a single API call
 final class RMRequest {
     
+    private struct Constants {
+        static let baseURL = "https://rickandmortyapi.com/api"
+    }
+    
+    private let method: HttpMethod = .get
+    private let endpoint: RMEndpoint
+    private let pathComponents: [String]
+    private let queryItems: [URLQueryItem]
+    
+    var url: URL? {
+        let string = pathComponents.reduce(Constants.baseURL + "/" + endpoint.rawValue) { $0 + "/" + $1 }
+        
+        guard let url = URL(string: string),
+              var components = URLComponents(url: url, resolvingAgainstBaseURL: true) else {
+            return nil
+        }
+        
+        if !queryItems.isEmpty {
+            components.queryItems = queryItems
+        }
+        
+        guard let url = components.url else {
+            return nil
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = method.rawValue
+        
+        return request.url
+    }
+    
+    init(endpoint: RMEndpoint, pathComponents: [String] = [], queryItems: [URLQueryItem] = []) {
+        self.endpoint = endpoint
+        self.pathComponents = pathComponents
+        self.queryItems = queryItems
+    }
 }
